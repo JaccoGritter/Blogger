68c39bcd633da71388b498b44da416459a156bd3
Jacco Gritter <jacco.gritter@gmail.com>
2019-07-16 15:41:32 +0200

    cosmetic changes to the buttons

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -9,10 +9,10 @@
         <p class="lead"> {{$blog->content}} </p>
     </div>
     <div class="ml-3 mr-2">
-        <a href="{{route('blogs.index')}}" class="btn btn-primary">Home</a>
+        <a href="{{route('blogs.index')}}" class="btn btn-outline-primary">Home</a>
     </div>
     <div>
-        <a href="{{route('blogs.edit', $blog->id)}}" class="btn btn-primary">Edit</a>
+        <a href="{{route('blogs.edit', $blog->id)}}" class="btn btn-outline-primary">Edit</a>
     </div>
 </div>
 
