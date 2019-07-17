0acd81213a846b7afeb9bb00736aa0ebcc393205
Jacco Gritter <jacco.gritter@gmail.com>
2019-07-17 10:55:27 +0200

    some styling added

--- resources/views/layouts/bloglayout.blade.php
+++ resources/views/layouts/bloglayout.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href= "{{ asset('css/blog.css') }}">
     <title>Blogger</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Holiday Blogger</a>
+  <img src="{{ asset('images/palm-tree-tree-silhouette.png') }}" alt="palm tree" id="palm">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -26,8 +28,10 @@
   </div>
 </nav>
 
-<div class="container">
-@yield('content')
+<div class="bg">
+  <div class="container">
+  @yield('content')
+  </div>
 </div>
 
 </body>
